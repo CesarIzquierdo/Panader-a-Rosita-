ec3c1f74cda647ef80ee396e9e9739fb792b9ab8
CesarIzquierdo <cesarizquierdo100@gmail.com>
2021-10-29 10:28:55 -0500

    version 1.2

---
+++ src/base/Conectar.java
@@ -0,0 +1,35 @@
+package base;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author cesar
+ */
+public class Conectar
+{
+
+    Connection enlazar = null;
+
+    public Connection conectar()
+    {
+        try
+        {
+
+            Class.forName("com.mysql.jdbc.Driver");
+            enlazar = DriverManager.getConnection("jdbc:mysql://localhost/panaderirosita", "root", "");
+
+        } catch (ClassNotFoundException ex)
+        {
+            System.out.println("error de " + ex);
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(Conectar.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return enlazar;
+    }
+}
